e4f967a4aa26ae946e0a815690a09d485fa50e02
Joe Skora <jskora@gmail.com>
2016-04-20 22:57:38 -0400

    Additional functionality and some OSX specific tweaks.

--- set-nifi-props.sh
+++ set-nifi-props.sh
@@ -1,24 +1,49 @@
 #!/bin/sh
 
+#--------------------------------------------------------------------------------
+# Configure Apache NiFi conf/bootstrap.conf and conf/nifi.properties files.
+#--------------------------------------------------------------------------------
+
 BOOTSTRAP=conf/bootstrap.conf
 PROPERTIES=conf/nifi.properties
 
+# configure based on host OS (assumes Linux)
+#------------------------------------------------------------
+
+OS=$(uname)
+if [ "$OS" == "" ]; then
+    OS=UNKNOWN
+fi
+
+# This uses .sbak extension for interim backups which are removed at completion.
+#
+# (The standard sed backup that made with "-i '.sbak'" option will not work for source
+# backup because sed is called multiple times on a file.  Also, OSX requires a parameter
+# to the "-i" option but behaves unexpectedly in shell expansions if given a blank
+# parameter ("-i ''") to suppresses backups,)
+SED="sed -i.sbak"
+
 # make backups
+#------------------------------------------------------------
 
 PREFIX=backup
 
 function backup {
     if [ ! -f $1.orig ]; then
         echo "[$PREFIX] ** backing up $1 to $1.orig"
+        echo "------------------------------------------------------------"
         cp -p $1 $1.orig
     else 
         echo "[$PREFIX] ** back up $1.orig already exists"
+        echo "------------------------------------------------------------"
     fi
 }
+
 backup $BOOTSTRAP
 backup $PROPERTIES
 
 # update nifi.properties
+#------------------------------------------------------------
 
 PREFIX=properties
 
@@ -29,20 +54,23 @@ if [ -z "$SKEY" ]; then
         SKEY="SKEY-$RANDOM-$RANDOM-$(date +%s)"
         echo "[$PREFIX] genreated key=$SKEY"
     fi
-    sed -i "s/nifi.sensitive.props.key=\$/nifi.sensitive.props.key=$SKEY/" $PROPERTIES
-    echo "[$PREFIX] ** sensitive properties key set to '$SKEY'"
+    echo "[$PREFIX] ** setting sensitive properties key to '$SKEY'"
+    echo "------------------------------------------------------------"
+
+    ${SED} "s/nifi.sensitive.props.key=\$/nifi.sensitive.props.key=$SKEY/" $PROPERTIES
 else
     echo "[$PREFIX] ** sensitive properties key already set to '$SKEY'"
 fi
 
 # update bootstrap.conf
+#------------------------------------------------------------
 
 PREFIX=bootstrap
 
 function option_on {
     if [ $(grep -E "^#$1=" $BOOTSTRAP) ]; then
         echo "[$PREFIX] turning on $1"
-        sed -i "s/^#$1=/$1=/" $BOOTSTRAP
+        ${SED} "s/^#$1=/$1=/" $BOOTSTRAP
     else
         echo "[$PREFIX] $1 already on"
     fi
@@ -51,7 +79,7 @@ function option_on {
 function option_off {
     if [ $(grep -E "^$1=" $BOOTSTRAP) ]; then
         echo "[$PREFIX] turning off $1"
-        sed -i "s/^$1=/#$1=/" $BOOTSTRAP
+        ${SED} "s/^$1=/#$1=/" $BOOTSTRAP
     else
         echo "[$PREFIX] $1 already off"
     fi
@@ -64,7 +92,23 @@ function add_option {
         echo "[$PREFIX] $NEW_OPT already exists"
     else
         echo "[$PREFIX] adding $NEW_OPT after $PRIOROPT"
-        sed -i "s#\(^$PRIOR_OPT$\)#\1\n$NEW_OPT#" $BOOTSTRAP
+        if [ "$OS" == "Darwin" ]; then
+            # Ugly OSX variant, but it works,
+            # (based on http://www.culmination.org/2008/02/sed-on-mac-os-x-105-leopard/)
+            # It boils down to changing what on Linux was
+            #     ...\n...
+            # to on OSX be
+            #     ...\\"$'\n'"...
+            # which appears to be creating
+            #     \\    - an escape character
+            #     "     - exit the quoted string
+            #     $'\n' - a literal \n character
+            #     "     - re-enter the quoted string
+            # Thus inserting a literal newline like on Linux but more awkwardly.
+            ${SED} "s#\(^$PRIOR_OPT$\)#\1\\"$'\n'"$NEW_OPT#" $BOOTSTRAP
+        else
+            ${SED} "s#\(^$PRIOR_OPT$\)#\1\n$NEW_OPT#" $BOOTSTRAP
+        fi
     fi
 }
 
@@ -77,6 +121,7 @@ ARG_AFTER="java.arg.14"
 for ON_ARG in $ARGS_ON; do
     option_on $ON_ARG
 done
+
 for OFF_ARG in $ARGS_OFF; do
     option_off $OFF_ARG
 done
@@ -86,3 +131,12 @@ for NEW_ARG in $ARGS_ADD; do
     add_option $AFTER $NEW_ARG
     AFTER=$(grep -E "^#?$NEW_ARG" $BOOTSTRAP)
 done
+
+# cleanup
+#------------------------------------------------------------
+
+rm conf/*.sbak
+
+#------------------------------------------------------------
+# end
+#------------------------------------------------------------
