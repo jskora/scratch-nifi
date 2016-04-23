df8f8586fad2c0a43e011a3968843836d370e98f
Joe Skora <jskora@gmail.com>
2016-04-23 15:32:09 -0400

    Add setup file.

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#------------------------------------------------------------
+# Install NiFi support files.
+#------------------------------------------------------------
+
+safe_copy () {
+    SRC=$1
+    DST=${HOME}/bin/$1
+    if [ -f ${DST} ]; then
+	cp -p ${DST} ${DST}.orig
+	echo "backup made ${DST} => ${DST}.orig"
+    fi
+    cp ${SRC} ${DST}
+    echo "installed ${SRC} as new ${DST}"
+}
+
+safe_copy setjava.sh
+safe_copy set-nifi-props.sh
+
+#--------------------------------------------------
+
+echo "$0 done"
+
+#============================================================
+# done
+#============================================================
