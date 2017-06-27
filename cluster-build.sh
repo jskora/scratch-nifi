ef37ef75f4ab7814fe034f57c2b021123c441e21
Joe Skora <jskora@apache.org>
2017-06-27 14:16:44 +0000

    Latest adjustments.

--- cluster-build.sh
+++ cluster-build.sh
@@ -10,10 +10,13 @@ NODE_OU="NIFI"
 NODE_HOST_PTRN="${CLUSTER_NAME}-${NODE_PREFIX}%02d"
 NODE_DN_PTRN="CN=${NODE_HOST_PTRN}, OU=NIFI"
 
+INIT_ADMIN="CN=testuser1, OU=${CLUSTER_NAME}-cluster"
+
+NIFI_USER=runnifi
 NIFI_NODES=5
 ZK_NODES=3
 
-NIFI_VER="nifi-1.3.0-SNAPSHOT"
+NIFI_VER="nifi-1.4.0-SNAPSHOT"
 
 NIFI_HTTPS_PORT="9443"
 NIFI_S2S_PORT="10443"
@@ -22,6 +25,8 @@ NIFI_ZK_PORT="2181"
 
 ZK_PORT_QUORUM="2182"
 ZK_PORT_ELECTION="2183"
+
+FIREWALL_PORTS=(${NIFI_HTTPS_PORT} ${NIFI_S2S_PORT} ${NIFI_CLUSTER_PORT} ${NIFI_ZK_PORT} ${ZK_PORT_QUORUM} ${ZK_PORT_ELECTION})
 
 JDK_VER="jdk1.8.0_121"
 JDK_BIN_SRC="java8/jdk-8u121-linux-x64.tar.gz"
@@ -35,11 +40,19 @@ DISK[2]="/dev/xvdc"
 # /etc/fstab line, device and mount point will be inserted
 FSTAB_PTRN="%s\t%s\tauto\tdefaults,nofail,noatime,comment=nifi\t0\t2"
 
+declare -A BOOTS
+BOOTS[java.arg.3]="-Xmx16384m"
+BOOTS[run.as]="${NIFI_USER}"
+
 declare -A PROPS
-PROPS[nifi.content.repository.directory]=/data1/content_repository
-PROPS[nifi.database.directory]=/data1/database_repository
-PROPS[nifi.flowfile.repository.directory]=/data2/flowfile_repository
-PROPS[nifi.provenance.repository.directory]=/data2/provenance_repository
+PROPS[nifi.content.repository.directory.default]="/data1/content_repository"
+PROPS[nifi.database.directory]="/data1/database_repository"
+PROPS[nifi.flowfile.repository.directory]="/data2/flowfile_repository"
+PROPS[nifi.provenance.repository.directory.default]="/data2/provenance_repository"
+PROPS[nifi.ui.banner.text]="NiFi - ${CLUSTER_NAME} cluster"  # node name added below
+PROPS[nifi.security.needClientAuth]="true"
+PROPS[nifi.cluster.is.node]="true"
+PROPS[nifi.cluster.flow.election.max.wait.time]="30 sec"
 
 # cluster independent script configuration
 #=================================================
@@ -73,7 +86,6 @@ CERTS_TEMP=/tmp/certs
 MOTD_FILE=/etc/motd
 
 ROOT_LOG_FILE=/root/install_build_tools.log
-FIREWALL_LOG=/root/install_firewall.log
 
 EPEL_REPO_FILE=/etc/yum.repos.d/epel.repo
 EPEL_GPG_KEY=/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
@@ -89,7 +101,6 @@ PROFILE_SCRIPT="/etc/profile.d/nifi-java.sh"
 
 NIFI_ROOT="${APP_ROOT}/${NIFI_VER}"
 NIFI_BIN_ENV="${NIFI_ROOT}/bin/nifi-env.sh"
-NIFI_USER=runnifi
 
 NIFI_BOOTSTRAP="${NIFI_ROOT}/conf/bootstrap.conf"
 NIFI_PROPS="${NIFI_ROOT}/conf/nifi.properties"
@@ -136,11 +147,13 @@ NODE_HOST=$(printf "${NODE_HOST_PTRN}" ${INST_INDEX})
 
 NODE_DN=$(printf "${NODE_DN_PTRN}" ${INST_INDEX})
 
-NIFI_ZK_CONNECT=""
-for IDX in $(1 ${ZK_NODES}); do
-    NIFI_ZK_CONNECT="${ZK_NODES},$(printf ${NODE_HOST_PTRN}, ${IDX}):${NIFI_ZK_PORT}"
+ZK_CONN=""
+for IDX in $(seq 1 ${ZK_NODES}); do
+    ZK_CONN="${ZK_CONN},$(printf ${NODE_HOST_PTRN} ${IDX}):${NIFI_ZK_PORT}"
 done
-NIFI_ZK_CONN=$(echo "${NIFI_ZK_CONN}" | sed "s/^,//")
+ZK_CONN=${ZK_CONN:1}
+
+PROPS[nifi.ui.banner.text]="NiFi - ${CLUSTER_NAME} cluster - node ${NODE_HOST}"
 
 LOG_HDR "starting ${INST_IP} $(date)"
 
@@ -149,9 +162,16 @@ LOG "   privateIp   =${INST_IP}"
 LOG "   instanceType=${INST_TYPE}"
 LOG "   imageId     =${INST_IMAGE}"
 LOG "   index       =${INST_INDEX}"
-LOG "   hostname was=${INST_HOST}"
-LOG "   hostname is =${NODE_HOST}"
-LOG "   nifi zk conn=${NIFI_ZK_CONNECT}"
+LOG "   hostname is =${INST_HOST}"
+LOG "   nodename is =${NODE_HOST}"
+LOG "   nifi zk conn=${ZK_CONN}"
+
+LOG "   nifi root   =${NIFI_ROOT}"
+LOG "   nifi-env.sh =${NIFI_BIN_ENV}"
+LOG "   nifi props  =${NIFI_PROPS}"
+LOG "   bootstrap   =${NIFI_BOOTSTRAP}"
+LOG "   authorizers =${NIFI_AUTHORIZERS}"
+LOG "   state-mgmt  =${NIFI_STATE_MANAGEMENT}"
 
 # add epel repo
 #=================================================
@@ -220,20 +240,6 @@ LOG_HDR "Installing other tools"
 
 yum install -y htop tree curl vim awscli nc
 
-# check in
-#=================================================
-
-cat <<MSG | nc 10.113.8.121 10080
-starting $(date)
-   instanceId=${INST_ID}
-   privateIp=${INST_IP}
-   instanceType=${INST_TYPE}
-   imageId=${INST_IMAGE}
-   hostname=${INST_HOST}
-   index=${INST_INDEX}
-   node=${NODE_HOST}
-MSG
-
 # Java
 #=================================================
 
@@ -246,12 +252,10 @@ else
     pushd /opt
 
     LOG "downloading ${JDK_BIN_SRC}"
-    JDK_BIN_SRC_URL="${S3_TOOLS}/${JDK_BIN_SRC}"
-    curl -s ${JDK_BIN_SRC_URL} | tar -xz
+    curl -s "${S3_TOOLS}/${JDK_BIN_SRC}" | tar -xz
 
     LOG "downloading ${JCE_BIN_SRC}"
-    JCE_BIN_SRC_URL="${S3_TOOLS}/${JCE_BIN_SRC}"
-    curl -s ${JCE_BIN_SRC_URL} | ${JDK_ROOT}/bin/jar x
+    curl -s "${S3_TOOLS}/${JCE_BIN_SRC}" | ${JDK_ROOT}/bin/jar x
     mv -f ${JCE_ROOT}/*.jar ${JDK_ROOT}/jre/lib/security/
     
     test -d ${JCE_ROOT} && rm -f ${JCE_ROOT}/*
@@ -262,7 +266,7 @@ else
 export JAVA_HOME=${JDK_ROOT}
 export PATH=\${JAVA_HOME}/bin:\${M2_HOME}/bin:${PATH}
 PROFILE
-    chmod go-w ${PROFILE_SCRIPT}
+    chmod 555 ${PROFILE_SCRIPT}
     MSG="Path has been configured for Java 8"
     grep -q "${MSG}" ${MOTD_FILE} || echo "${MSG}" >> ${MOTD_FILE}
 
@@ -275,29 +279,22 @@ fi
 
 LOG_HDR "opening firewall ports"
 
-open_port() {
-    LOG "checking port $1"
+for PORT in ${FIREWALL_PORTS[@]}; do
+    LOG "checking port ${PORT}"
     setenforce 0
-    firewall-cmd --list-all 2>&1 | tee -a ${FIREWALL_LOG}.${1}
-    PORT_STATUS=$(firewall-cmd --zone=public --query-port=$1/tcp)
+    firewall-cmd --list-all 2>&1
+    PORT_STATUS=$(firewall-cmd --zone=public --query-port=${PORT}/tcp)
     setenforce 1
     if [ "${PORT_STATUS}" == "no" ]; then
         setenforce 0
-        firewall-cmd --zone=public --add-port=$1/tcp --permanent 2>&1 | tee -a ${FIREWALL_LOG}.${1}
-        firewall-cmd --reload 2>&1 | tee -a ${FIREWALL_LOG}.${1}
+        firewall-cmd --zone=public --add-port=${PORT}/tcp --permanent
+        firewall-cmd --reload 2>&1
         setenforce 1
-        LOG "firewall port $1 opened"
+        LOG "firewall port ${PORT} opened"
     else
-        LOG "firewall port $1 not opened, already open"
+        LOG "firewall port ${PORT} not opened, already open"
     fi
-}
-
-open_port ${NIFI_HTTPS_PORT}
-open_port ${NIFI_S2S_PORT}
-open_port ${NIFI_CLUSTER_PORT}
-open_port ${NIFI_ZK_PORT}
-open_port ${ZK_PORT_QUORUM}
-open_port ${ZK_PORT_ELECTION}
+done
 
 # nifi
 #=================================================
@@ -305,36 +302,48 @@ open_port ${ZK_PORT_ELECTION}
 LOG_HDR "downloading and configuring NiFi"
 
 set_prop() {
-    LOG "setting ${1} ${2}=${3}"
+    LOG "      setting ${1} ${2}=${3}"
     if grep -q "^${2}=" ${1} ; then
-        sed -ir "s/^\(${2}\)=.*$/\1=${3}\n/" ${1}
-        LOG "updated"
+        sed -i -r "s@^(${2})=.*@\1=${3}@" ${1}
+        LOG "         updated"
     else
+        # add newline at EOF if missing
+        if [ -n "$(tail -c 1 ${1})" ]; then
+            LOG "         newline added at EOF"
+            echo "" >> ${1}
+        fi
         echo "${2}=${3}" >> ${1}
-        LOG "added"
+        LOG "         added"
     fi
 }
 
 set_xml() {
-    LOG "setting ${1} ${2}=${3}"
-    sed -ir "s@\(<property name=\"${2}\">\).*\(</property>\)@\1${3}\2@" ${1}
+    LOG "      setting ${1} ${2}=${3}"
+    sed -i -r "s@(<property name=\"${2}\">).*(</property>)@\1${3}\2@" ${1}
 }
+
+if ! getent passwd ${NIFI_USER}; then
+    LOG "adding nifi user"
+    useradd -M ${NIFI_USER}
+fi
 
 if [ ! -d ${NIFI_ROOT} ]; then
 
     LOG "   download and uncompress nifi"
-
+    
     NIFI_BIN_SRC_URL="${S3_BUILDS}/${NIFI_BIN_SRC}"
+    LOG "${NIFI_BIN_SRC_URL}"
     mkdir -p ${NIFI_ROOT}
     pushd ${NIFI_ROOT}
-    LOG "${NIFI_BIN_SRC_URL}"
     curl -s ${NIFI_BIN_SRC_URL} | tar --strip-components=1 -xz
     popd
+    cp -p -r ${NIFI_ROOT}/conf ${NIFI_ROOT}/conf.back-pre-config
     LOG "$(ls -l ${NIFI_ROOT})"
 
     LOG "   download certs and copy keys and property files into place"
 
     CERTS_BIN_SRC_URL="${S3_BUILDS}/${CERTS_BIN_SRC}"
+    LOG "${CERTS_BIN_SRC_URL}"
     rm -rf ${CERTS_TEMP}
     mkdir ${CERTS_TEMP}
     pushd ${CERTS_TEMP}
@@ -344,71 +353,66 @@ if [ ! -d ${NIFI_ROOT} ]; then
     cp -p ${CERTS_TEMP}/$(printf ${NODE_HOST_PTRN} ${INST_INDEX})/* ${NIFI_ROOT}/conf/
     LOG "$(ls -l ${NIFI_ROOT}/conf)"
 
-    if [[ "${NIFI_VERSION}" = *"1.3.0"* ]]; then
-        # temp fix
-        set_prop "${NIFI_PROPS}" nifi.security.user.authorizer file-provider
-    fi
-
     LOG "   bootstrap.conf"
 
-    set_prop "${NIFI_BOOTSTRAP}" "java.args.3" "-Xmx16384m"
-    set_prop "${NIFI_BOOTSTRAP}" "run.as" "${NIFI_USER}"
-    
+    for KEY in ${!BOOTS[@]}; do
+        VALUE=${BOOTS[$KEY]}
+        set_prop "${NIFI_BOOTSTRAP}" "${KEY}" "${VALUE}"
+    done
+
     LOG "   nifi.properties"
 
-    LOG "      banner"
-    set_prop "${NIFI_PROPS}" "nifi.ui.banner.text" "${CLUSTER_NAME}"
+    for KEY in ${!PROPS[@]}; do
+        VALUE=${PROPS[$KEY]}
+        set_prop "${NIFI_PROPS}" "${KEY}" "${VALUE}"
+    done
 
-    LOG "      client auth"
-    set_prop "${NIFI_PROPS}" "nifi.security.needClientAuth" "true"
-
-    LOG "      cluster"
-    set_prop "${NIFI_PROPS}" "nifi.cluster.is.node" "true"
-
-    LOG "      embedded zookeeper (for first ZK_NODES nodes)"
     if [ ${INST_INDEX} -le ${ZK_NODES} ]; then
         set_prop "${NIFI_PROPS}" "nifi.state.management.embedded.zookeeper.start" "true"
         mkdir -p ${NIFI_ROOT}/state/zookeeper
         echo ${INST_INDEX} > ${ZK_MYID}
     fi
 
-    LOG "      other properties"
-    for KEY in ${!PROPS[@]}; do
-        VALUE=${PROPS[KEY]}
-        LOG "         ${KEY}=${VALUE}"
-        set_prop "${NIFI_PROPS}" "${KEY}" "${VALUE}"
-    done
+    set_prop "${NIFI_PROPS}" "nifi.zookeeper.connect.string" "${ZK_CONN}"
 
     LOG "   zookeeper.properties"
 
     for IDX in $(seq 1 ${ZK_NODES}); do
         NODE_NAME="$(printf "${NODE_HOST_PTRN}" ${IDX})"
-        set_prop "${ZK_PROPS}" server.${IDX} ${NODE_NAME}:${ZK_PORT_QUORUM}:${ZK_PORT_ELECTION}
+        set_prop "${ZK_PROPS}" "server.${IDX}" "${NODE_NAME}:${ZK_PORT_QUORUM}:${ZK_PORT_ELECTION}"
     done
 
-    set_xml "${NIFI_AUTHORIZERS}" "Initial Admin Identity" "CN=testuser1, OU=nifidev"
-    grep "Initial Admin Identity" ${NIFI_AUTHORIZERS}
+    LOG "   authorizers.xml"
 
-    PROP_PTRN="<property name=\"Node Identity %d\">CN=%s, OU=NIFI</property>"
+    # userGroupProvider
+    set_xml "${NIFI_AUTHORIZERS}" "Initial User Identity 1" "${INIT_ADMIN}"
+    PROP_PTRN="<property name=\"Initial User Identity node%d\">CN=%s, OU=NIFI</property>"
     SVRS=""
-    CONNECT=""
     for IDX in $(seq 1 ${NIFI_NODES}); do
         NODE_NAME="$(printf "${NODE_HOST_PTRN}" ${IDX})"
         SVRS="${SVRS}$(printf "${PROP_PTRN}" "${IDX}" "${NODE_NAME}")\n"
-        if [ $((${IDX})) -le $((${ZK_NODES})) ]; then
-            CONNECT="${CONNECT},${NODE_NAME}:${NIFI_ZK_PORT}"
-        fi
     done
-    sed -ir "s@    </authorizer>@${SVRS}    </authorizer>@" ${NIFI_AUTHORIZERS}
-    CONNECT=$(echo "${CONNECT}" | sed "s/^,//")
-    set_prop "${NIFI_PROPS}" nifi.zookeeper.connect.string "${CONNECT}"
+    sed -i "s@    </userGroupProvider>@${SVRS}    </userGroupProvider>@" ${NIFI_AUTHORIZERS}
+    LOG "adding userGroupProvider host identities: ${SVRS}"
+
+    # accessPolicyProvider 
+    set_xml "${NIFI_AUTHORIZERS}" "Initial Admin Identity" "${INIT_ADMIN}"
+    PROP_PTRN="<property name=\"Node Identity node%d\">CN=%s, OU=NIFI</property>"
+    SVRS=""
+    for IDX in $(seq 1 ${NIFI_NODES}); do
+        NODE_NAME="$(printf "${NODE_HOST_PTRN}" ${IDX})"
+        SVRS="${SVRS}$(printf "${PROP_PTRN}" "${IDX}" "${NODE_NAME}")\n"
+    done
+    sed -i "s@    </accessPolicyProvider>@${SVRS}    </accessPolicyProvider>@" ${NIFI_AUTHORIZERS}
+    LOG "adding accessPolicyProvider host identities: ${SVRS}"
 
     LOG "   state-management.xml"
-    set_xml "${NIFI_STATE_MANAGEMENT}" "Connect String" "${CONNECT}"
+    set_xml "${NIFI_STATE_MANAGEMENT}" "Connect String" "${ZK_CONN}"
 
     LOG "      ${NIFI_BIN_ENV} - force JDK"
-    sed -ir "s@#export JAVA_HOME=.*@export JAVA_HOME=${JDK_ROOT}@" ${NIFI_BIN_ENV}
+    sed -i "s@#export JAVA_HOME=.*@export JAVA_HOME=${JDK_ROOT}@" ${NIFI_BIN_ENV}
 
+    cp -p -r ${NIFI_ROOT}/conf ${NIFI_ROOT}/conf.back-post-config
     LOG "NIFI downloaded and configured"
 else
     LOG "NIFI download and configure skipped, already exists"
@@ -418,21 +422,6 @@ fi
 #=================================================
 
 LOG_HDR "Final system configuration"
-
-LOG "   /etc/hosts"
-for IDX in $(seq 1 ${NIFI_NODES}); do
-    NODE_NAME="$(printf "${NODE_HOST_PTRN}" ${IDX})"
-    LOG "      NODE_NAME=${NODE_NAME}"
-    if ! grep -q ${NODE_NAME} /etc/hosts ; then
-        if [ "${IDX}" == "${INST_INDEX}" ]; then
-            echo -e "${INST_IP}\t${NODE_NAME}" >> /etc/hosts
-        else
-            echo -e "0.0.0.0\t${NODE_NAME}" >> /etc/hosts
-        fi
-    else
-	LOG "      /etc/hosts entry found, skipping"
-    fi
-done
 
 LOG "   configuring data volume(s)"
 for IDX in ${!DISK[@]}; do
@@ -444,29 +433,31 @@ for IDX in ${!DISK[@]}; do
         mkfs.ext4 ${DISK}
         mkdir ${DISK_DIR}
         chown -R ${NIFI_USER}: ${DISK_DIR}
-        chmod -R u+rwx,g+rws,o- ${DISK_DIR}
+        chmod -R 770 ${DISK_DIR}
 
-        sed -ir "s@^${DISK}@#${DISK}@" /etc/fstab
-        printf "${FSTAB_PTRN}" "${DISK}" "${DISK_DIR}" >> /etc/fstab
+        sed -i "s@^${DISK}@#${DISK}@" /etc/fstab
+        printf "${FSTAB_PTRN}\n" "${DISK}" "${DISK_DIR}" >> /etc/fstab
 	    mount ${DISK}
+        chown -R ${NIFI_USER}: ${DISK_DIR}
+        chmod -R 770 ${DISK_DIR}
     fi
 done
 
-# nifi user and service
+# nifi service
 #=================================================
 
-LOG "adding nifi user and service"
+LOG "adding nifi service"
 
 LOG "   adding user '${NIFI_USER}'"
 useradd -M ${NIFI_USER}
 
 LOG "   setting ownership of JDK '${JDK_ROOT}'"
 chown -R root: ${JDK_ROOT}
-chmod -R 755 ${JDK_ROOT}
+chmod -R 555 ${JDK_ROOT}
 
 LOG "   setting ownership of NiFi '${NIFI_ROOT}'"
 chown -R ${NIFI_USER}: ${NIFI_ROOT}
-chmod -R u+rwx,g+rws,o- ${NIFI_ROOT}
+chmod -R 770 ${NIFI_ROOT}
 
 LOG "   adding nifi service"
 pushd ${NIFI_ROOT}
@@ -478,23 +469,28 @@ popd
 #=================================================
 
 LOG "wrapping up"
-MSG=$(cat <<NOTES
-NOTE: after the system starts up, the following tasks need to be done.
-  1 - add appropriate users to 'runnifi' group.
-          \$ sudo usermod -a -G runnifi <userid>
-  2 - populate all nodes' /etc/hosts with IPs of all the hosts
-        - accumulate all /root/hosts-* files and append all node IPs to each node's /etc/hosts file.
-              \$ for IP in IP1 [IP2 ...]; do
-                scp -p \$IP:/tmp/host-entry-* /tmp
-              done
-              \$ for ENTRY_FILE in /tmp/host-entry-*; do
-                NODE_ENTRY=\$(cat \${ENTRY_FILE})
-                if ! grep -q \${NODE_ENTRY} /etc/hosts ; then
-                  cat \${ENTRY_FILE} >> /etc/hosts
-                fi
-              done
-NOTES)
-LOG "$MSG"
+
+ADD_GROUP_SCRIPT="/root/add_user_to_group_runnifi.sh"
+cat <<ADD_GROUP > ${ADD_GROUP_SCRIPT}
+#!/bin/bash
+usermod -a -G ${NIFI_USER} \$1
+ADD_GROUP
+chmod +x ${ADD_GROUP_SCRIPT}
+
+HOSTS_FILE="/etc/hosts"
+MAKE_HOSTS_SCRIPT="/root/make_hosts.sh"
+cat <<MAKE_HOSTS > ${MAKE_HOSTS_SCRIPT}
+#!/bin/bash
+while [ "\$1" != "" ]; do
+    IP="\$1"
+    HOST="\$2"
+    echo -e "\$IP\t\$HOST" >> ${HOSTS_FILE}
+    shift 2
+done
+MAKE_HOSTS
+chmod +x ${MAKE_HOSTS_SCRIPT}
+
+LOG "$(echo -e '\n/etc/hosts\n--------------------'; cat /etc/hosts; echo -e '\nadd group script\n--------------------'; md5sum ${ADD_GROUP_SCRIPT}; wc ${ADD_GROUP_SCRIPT}; cat ${ADD_GROUP_SCRIPT}; echo -e 'make hosts script\n--------------------'; md5sum ${MAKE_HOSTS_SCRIPT}; wc ${MAKE_HOSTS_SCRIPT}; cat ${MAKE_HOSTS_SCRIPT})"
 
 #=================================================
 
